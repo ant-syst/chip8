7049940546fd9ef9039073f4b4f8a600f71e3c69
Antoine BLIN <git.ant.git>
2017-11-13 00:22:11 +0100

    Parsing options

--- src/args.c
+++ src/args.c
@@ -2,15 +2,19 @@
 
 #include <string.h>
 
-int parse_name(int argc, char ** argv, char const *name)
+static int lookup_name_index(int argc, char ** argv, char const *name)
 {
     int i;
 
     for(i=1; i<argc; i++)
         if(strcmp(name, argv[i]) == 0)
-            return 0;
+            return i;
+    return -1;
+}
 
-    return 1;
+int parse_name(int argc, char ** argv, char const *name)
+{
+    return lookup_name_index(argc, argv, name) != -1;
 }
 
 char const * parse_str(int argc, char ** argv, char const *name, char const *def)
@@ -20,6 +24,19 @@ char const * parse_str(int argc, char ** argv, char const *name, char const *def
     for(i=1; i<argc-1; i++)
         if(strcmp(name, argv[i]) == 0)
             return argv[i + 1];
+
+    return def;
+}
+
+char const * parse_choices(int argc, char ** argv, char const *name, char const ** choices, char const *def)
+{
+    int index = lookup_name_index(argc, argv, name);
+    if(index == -1 || index == argc-1)
+        return def;
+
+    for(; *choices != NULL; choices++)
+        if(strcmp(argv[index + 1], *choices) == 0)
+            return *choices;
 
     return def;
 }
--- src/args.h
+++ src/args.h
@@ -5,4 +5,6 @@ int parse_name(int argc, char ** argv, char const *name);
 
 char const * parse_str(int argc, char ** argv, char const *name, char const *def);
 
+char const * parse_choices(int argc, char ** argv, char const *name, char const ** choices, char const *def);
+
 #endif
