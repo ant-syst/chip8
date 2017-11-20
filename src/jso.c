2f45dbe63185ac770754324894de4e93d111bab5
Antoine BLIN <git.ant.git>
2017-11-20 01:11:35 +0100

    Unit test creation for jso

--- src/jso.c
+++ src/jso.c
@@ -133,37 +133,3 @@ int _jso_av(struct jso_ctx * ctx, char const * value, int i_str, ...)
 
     return 0;
 }
-/*
-int main(void)
-{
-    struct jso_ctx ctx = JSO_CTX_INIT(stdout);
-
-    jso_bd(&ctx, error);
-    jso_kvs(&ctx, error, "test", "help");
-    jso_kvs(&ctx, error, "test2", "help2");
-    jso_kvo(&ctx, error, "test2", "2");
-
-    jso_k(&ctx, error, "inner");
-    jso_bd(&ctx, error);
-    jso_kvs(&ctx, error, "test", "help");
-    jso_kvs(&ctx, error, "test", "help");
-    jso_kvs(&ctx, error, "test2", "help2");
-    jso_ed(&ctx, error);
-
-    jso_k(&ctx, error, "array");
-    jso_ba(&ctx, error);
-    jso_avs(&ctx, error, "test1");
-    jso_avs(&ctx, error, "test2");
-    jso_avo(&ctx, error, "2");
-    jso_ea(&ctx, error);
-
-    jso_ed(&ctx, error);
-
-    printf("\n");
-
-    return 0;
-
-    error:
-
-    return -1;
-}*/
